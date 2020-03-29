c9d460d77e5283db7eaa20f57a3cc2b4948fd22b
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-29 16:32:09 +0700

    allowed user to add new comments to an issue and reload a current comments

--- src/App.js
+++ src/App.js
@@ -120,6 +120,7 @@ function App() {
             return (
                 <EveryIssue 
                     selectedIssue = {selectedIssue}
+                    token = {token}
                 />
             )
         }
--- src/components/EveryIssue.js
+++ src/components/EveryIssue.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal';
+const ReactMarkdown = require('react-markdown/with-html')
 
 
 export default function EveryIssue(props) {
+
     const [comments, setComments] = useState([]);
+    const [newCommentText, setNewCommentText] = useState('');
+    const [loading,setLoading] = useState (false);
+
     const fetchComments = async () => {
         let url = props.selectedIssue.comments_url;
         let respone = await fetch(url, {
             method: 'GET',
-            // headers: {
-            //     'Content-Type': 'application/vnd.github.mercy-preview+json'
-            // },
+
         })
         let result = await respone.json()
-        setComments (result)
+        setComments(result)
     }
     useEffect(() => {
         fetchComments()
     }, [])
-    console.log('selectedIssue', props.selectedIssue);
 
     return (
         <div>
@@ -27,32 +29,60 @@ export default function EveryIssue(props) {
                 <p> number: {props.selectedIssue.number} + Title: {props.selectedIssue.title} </p>
 
                 <p> State: {props.selectedIssue.state}</p>
-                <div dangerouslySetInnerHTML={{ __html: props.selectedIssue.body }} />
-                
-                <p> Comment: {props.selectedIssue.comments} </p>
 
-                
-                
-                {comments.map((item)=>{
+                <ReactMarkdown
+                    source={props.selectedIssue.body}
+                    escapeHtml={false}
+                />
+
+                <input value={newCommentText} onChange={(event) => {
+                    setNewCommentText(event.target.value)
+                }} />
+                <button onClick={async () => {
+                    setLoading (true);
+
+                    let url = props.selectedIssue.comments_url;
+                    let respone = await fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+
+                        },
+
+                        body: JSON.stringify({ body: newCommentText })
+
+                    })
+                    let result = await respone.json()
+
+                    setTimeout(()=>{
+                        fetchComments().then(()=>{
+                            setLoading(false)
+                        })
+                    },3000)
+                }}> Add New Comment </button>
+                <button onClick={()=>{
+                    setLoading(true);
+                    fetchComments().then(()=>{
+                        setLoading(false)
+                    })
+                }}> Reload Comment </button>
+                <p> Comment: {props.selectedIssue.comments} </p>
+                {loading?"loading...":comments.map((item) => {
+
                     return (
-                    <div key={item.id} dangerouslySetInnerHTML={{ __html: item.body }} />
+
+                        <div style={{ border: "2px solid black" }} key={item.id}>
+                            <ReactMarkdown
+                                source={item.body}
+                                escapeHtml={false}
+                            />
+                        </div>
+
                     )
                 })}
 
-
-
-                {/* <Modal
-                    isOpen={props.modalIsOpen}
-                    onRequestClose={props.closeModal}
-                    style={{ overlay: { display: "flex", justifyContent: "center" }, content: { width: "70%", height: "70%", position: "relative" } }}
-                    contentLabel="Example Modal"
-                >
-                    <div onClick={() => props.openModal()}>
-                        <p>{props.issues.number}</p>
-                        <p> {props.issues.title}</p>
-
-                    </div>
-                </Modal> */}
             </div>
         </div>
     )
