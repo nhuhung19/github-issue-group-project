4cb7744aa39a23d6d6b266563edc0d80b233e56d
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-29 20:48:02 +0700

    working

--- src/components/EveryIssue.js
+++ src/components/EveryIssue.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal';
 const ReactMarkdown = require('react-markdown/with-html')
 
-
 export default function EveryIssue(props) {
 
     const [comments, setComments] = useState([]);
@@ -13,7 +12,6 @@ export default function EveryIssue(props) {
         let url = props.selectedIssue.comments_url;
         let respone = await fetch(url, {
             method: 'GET',
-
         })
         let result = await respone.json()
         setComments(result)
@@ -34,13 +32,11 @@ export default function EveryIssue(props) {
                     source={props.selectedIssue.body}
                     escapeHtml={false}
                 />
-
                 <input value={newCommentText} onChange={(event) => {
                     setNewCommentText(event.target.value)
                 }} />
                 <button onClick={async () => {
                     setLoading (true);
-
                     let url = props.selectedIssue.comments_url;
                     let respone = await fetch(url, {
                         method: 'POST',
@@ -48,26 +44,24 @@ export default function EveryIssue(props) {
                             'Accept': 'application/json',
                             'Content-Type': 'application/json',
                             Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-
                         },
-
                         body: JSON.stringify({ body: newCommentText })
-
                     })
                     let result = await respone.json()
-
                     setTimeout(()=>{
                         fetchComments().then(()=>{
                             setLoading(false)
                         })
                     },3000)
                 }}> Add New Comment </button>
+
                 <button onClick={()=>{
                     setLoading(true);
                     fetchComments().then(()=>{
                         setLoading(false)
                     })
                 }}> Reload Comment </button>
+
                 <p> Comment: {props.selectedIssue.comments} </p>
                 {loading?"loading...":comments.map((item) => {
 
